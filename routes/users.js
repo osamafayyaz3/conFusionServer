3b02489c8e8d0afff87e92a99156d1a11d2ac9d5
osamafayyaz3 <osama85@my.yorku.ca>
2020-08-13 19:11:15 -0400

    mongoose population

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+//5f35c7c1046c568c3e6b448e
+
 var express = require('express');
 const bodyParser = require('body-parser');
 const User = require('../models/user');
@@ -21,11 +23,26 @@ router.post('/signup', (req, res, next) => {
         res.json({ err: err });
       }
       else {
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({ success: true, status: 'Registration Successful!' });
-        });
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
+        }
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ err: err });
+            return ;
+          }
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ success: true, status: 'Registration Successful!' });
+          });
+        })
+
       }
     });
 });
