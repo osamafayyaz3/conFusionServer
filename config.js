34c39c38b4bcc0f8c5c07afd3f35914c9b6a2875
Osama Fayyaz Ahmad <49039028+osamafayyaz3@users.noreply.github.com>
2022-07-13 19:59:17 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '299518194650941',
-        clientSecret: 'a73877434a80d0e70c1d8d9a2423074d'
+        clientId: 'xx',
+        clientSecret: 'xx'
     }
 }
